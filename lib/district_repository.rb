008233fdaf468f2f8b3e8a11097c4208989ebd7a
blackknight75 <devbox4714@gmail.com>
2017-01-05 11:37:18 -0700

    District repository complete and refactored for iterations 0

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -3,42 +3,25 @@ require './lib/district'
 class DistrictRepository
   attr_reader :districts
 
-  def initialize(districts = {})
-    @districts = districts
+  def initialize
+    @districts = Hash.new(0)
   end
 
   def load_data(path)
     filename = path[:enrollment][:kindergarten]
-
     CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
-      name = row[:location]  # row[0]
-      #data = row[:data]      # row[3]
-      #year = row[:timeframe] # row[1]
-
+      name = row[:location]
       d = District.new({:name => name})
-
-      @districts[name] = d
-
+      @districts[name.to_sym] = d
     end
   end
 
   def find_by_name(name)
-    search_districts(name)
+    @districts[(name.upcase.to_sym)] if @districts[(name.upcase.to_sym)] != 0
   end
 
-  # def find_all_matching(names)
-  #   all_matching = []
-  #   names.each do |name|
-  #     all_matching << find_by_name(name)
-  #   end
-  #   all_matching
-  # end
-
-  def search_districts(name)
-    @districts.each do |district|
-      return district[1] if (name.upcase) == district[1].name
-      # return nil if (name.upcase) != district[1].name
-    end
+  def find_all_matching(names)
+    found_districts = names.map {|name| find_by_name(name)}.compact
   end
 
 end
